cef001eed8a4b3be1271efe9022d2b1b2c7529fc
ilya_the_creator <respondatanytime@gmail.com>
2022-01-27 18:49:16 +0300

    Feature/weather 1 (#1)
    * сверстал city list page
    
    * сверстал city page
    
    * сверстал settings page
    
    * добавил ссылки на имеющиеся страницы, сделал составной модификатор для bg
    
    * исправил стили для button/add-btn
    
    * grid разметка на странице города
    
    * убрал float у кнопки
    
    * привел элементы на странице настроек к размерам в соответствии с дизайном
    
    * шрифты
    
    * small fixes: styles are in multiple files, icons and styles are correct now
    
    * added feature of finding out device type in js
    
    * extended device detection functionality
    
    * added comments to js
    
    * fixed device width issue + added z-index to navigation so it is always on top
    
    * fixed add button size and border-radius for widgets
    
    * fixed cities' temperature font sizes
    
    * imported and used svg icons from figma
    
    * edited .gitignore
    
    * adaptive css fixes
    
    * added basier-circle font
    
    * made markup as close to the design as possible

--- app/js/index.js
+++ app/js/index.js
@@ -1,19 +1,158 @@
-console.log("hello");
-console.log("I am working!");
+/*
+  Array which allows us to loop through device types
 
-const dummyFunc = () => {
-  const a = 2;
-  const b = 43;
+  overall structure: [ [], [], [] ] - array of arrays. Each inner array is a group of related operating systems (e.g. mac and ios)
+  structure of inner array: [ {}, {} ] - one or two objects. Each object represents a particular OS
+  structure of the OS object: { systemName, devices } - devices - list of device types available in that OS
+  structure of devices: [ {}, {} ] - each object is a separate device 
+  structure of the device: { name, types } - types - array of strings which will help us to search through userAgent string 
+*/
+const devices = [
+  [
+    {
+      systemName: "android",
+      devices: [
+        {
+          name: "device",
+          types: [],
+        },
+      ],
+    },
+  ],
+  [
+    {
+      systemName: "windows",
+      devices: [
+        {
+          name: "mobile",
+          types: ["Mobile"],
+        },
+        {
+          name: "desktop",
+          types: ["win64", "wow64;", "wow64", "win64;"],
+        },
+      ],
+    },
+  ],
+  [
+    {
+      systemName: "ios",
+      devices: [
+        {
+          name: "device",
+          types: ["iphone;", "iphone"],
+        },
+      ],
+    },
+    {
+      systemName: "mac",
+      devices: [
+        {
+          name: "desktop",
+          types: ["macintosh", "macintosh;"],
+        },
+        {
+          name: "tablet",
+          types: ["ipad", "ipad;"],
+        },
+      ],
+    },
+  ],
+  [
+    {
+      systemName: "linux",
+      devices: [
+        {
+          name: "device",
+          types: [],
+        },
+      ]
+    }
+  ]
+];
 
-  return a + b;
+// Array which allows us to loop through different device sizes
+const deviceDimensions = [
+  {
+    name: "phone-wide",
+    maxWidth: 599,
+  },
+  {
+    name: "tablet-portrait-wide",
+    maxWidth: 600,
+  },
+  {
+    name: "tablet-landscape-wide",
+    maxWidth: 900,
+  },
+  {
+    name: "desktop-wide",
+    maxWidth: 1200,
+  },
+  {
+    name: "big-desktop-wide",
+    maxWidth: 1920,
+  },
+  {
+    name: "large-desktop-wide",
+    maxWidth: 2600,
+  },
+];
+
+/* 
+  Function which in the end assigns a class to outer wrapper of the application which tells 
+  what device a user is in.
+*/
+const detectDevice = () => {
+  const ua = navigator.userAgent.toLocaleLowerCase();
+  let typeClassName;
+
+  devices.forEach(systemGroup => {
+    systemGroup.forEach(system => {
+      system.devices.forEach(device => {
+        let searchResult;
+
+        // If there are no types for this particular device, do the search with systemName (windows, linux, android, etc)
+        if (device.types.length === 0) {
+          searchResult = ua.search(system.systemName);
+        } 
+
+        // Otherwise, search for each individual type is executed
+        device.types.forEach(type => {
+          searchResult = ua.search(type);
+        })
+
+        // If there's a match in the search, assign this value to the class variable
+        if (searchResult !== -1) {
+          typeClassName = `screen-wrapper--${system.systemName}--${device.name}`;
+        }
+
+        /* 
+          Expections.
+          If you are on android, your userAgent will have both linux and android strings matches.
+          Here typeClassName is explicitly set to the value of android
+        */
+        if (ua.search("linux") !== -1 && ua.search("android") ) {
+          typeClassName = "screen-wrapper--android--device";
+        }
+      })
+    });
+  });
+
+  document.querySelector(".screen-wrapper").classList.add(typeClassName);
 };
 
-const list = [1, 2, 3, 4, 5];
-const newList = [...list, 6, 7];
+const detectDeviceWidth = () => {
+  for (const dimension of deviceDimensions) {
+    // if there's a match, assign the class and exit from the loop
+    if (screen.width <= dimension.maxWidth) {
+      document.querySelector(".screen-wrapper").classList.add(dimension.name);
+      break;
+    } 
+  }
+}
 
-const obj = {
-  key1: 333,
-  key2: 23,
+window.onload = () => {
+  detectDevice();
+  detectDeviceWidth();
 };
-
-const newObj = { ...obj, name: "what?" };
