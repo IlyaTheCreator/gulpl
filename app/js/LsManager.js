6445d185214589ddb85bca7b5b89e1bcb1acedf5
ilyaTheCreator <respondatanytime@gmail.com>
2022-01-28 13:54:08 +0300

    added localStorage manager class

---
+++ app/js/LsManager.js
@@ -0,0 +1,55 @@
+export default class LsManager {
+    appKey = "";
+
+    init(appKey, data = {}) {    
+        if (!localStorage.getItem(appKey)) {
+            localStorage.setItem(appKey, JSON.stringify(data));
+        }
+
+        this.appKey = appKey;
+    }
+
+    get(key) {
+        return JSON.parse(localStorage.getItem(this.appKey))[key];
+    }
+
+    set(key, value) {
+        const newData = JSON.parse(localStorage.getItem(this.appKey));
+
+        newData[key] = value;
+        localStorage.setItem(this.appKey, JSON.stringify(newData));
+    }
+
+    delete(key) {
+        const newData = JSON.parse(localStorage.getItem(this.appKey));
+
+        delete newData[key];
+        localStorage.setItem(this.appKey, JSON.stringify(newData));
+    }
+
+    clear() {
+        localStorage.setItem(this.appKey, JSON.stringify({}));
+    }
+
+    static list() {
+        const output = {};
+
+        for (let jsonItem in localStorage) {
+            if (jsonItem === "length") {
+                break;
+            }
+
+            const item = JSON.parse(localStorage.getItem(jsonItem))
+
+            for (let key in item) {
+                output[key] = JSON.parse(localStorage.getItem(jsonItem))[key];
+            }
+        }
+
+        return output;
+    }
+
+    static reset() {
+        localStorage.clear();
+    }
+}
--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,28 @@
 import addWrapperClass from "./wrapperClassScript";
+import LsManager from "./LsManager";
 
 // Adding css class to screen-wrapper based on device operating system and width
 addWrapperClass();
+
+// Working with localStorage 
+// const lsManager = new LsManager();
+
+// lsManager.init("weather", {
+//     keyFirst: false,
+//     keySecond: false,
+//     keyThird: false,
+//     keyFourth: false
+// });
+
+// lsManager.set("keyFirst", "valueFirst");
+// lsManager.set("keySecond", "valueSecond");
+// lsManager.set("keyThird", "valueThird");
+// lsManager.set("keyFourth", "valueFourth");
+
+// lsManager.delete("keySecond");
+// // alert(lsManager.get("keySecond"));
+
+// console.log(LsManager.list());
+// // lsManager.clear();
+
+// // LsManager.reset();
