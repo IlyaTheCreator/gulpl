9f1d4b63e6d1359d3ae7aa29dd5403e8c9aafc4e
ilyaTheCreator <respondatanytime@gmail.com>
2022-02-16 17:24:34 +0300

    test 123

--- app/js/classes/DashBoard.js
+++ app/js/classes/DashBoard.js
@@ -16,14 +16,14 @@ export default class DashBoard {
    */
   createContentWrapper(city) {
     const contentWrapper = document.createElement("div");
+    const i = 2;
 
     contentWrapper.innerHTML = `
       <h1 class="screen__header">${city.title}</h1>
       <p class="screen__date">${new Date(city.date).toDateString()}</p>
       <div class="screen__weather">
-          <img src="${
-            city.cityImage
-          }" alt="weather image" class="screen__image">
+          <img src="${city.cityImage
+      }" alt="weather image" class="screen__image">
           <p class="screen__temperature">${city.currentTemp}°</p>
           <p class="screen__weather-type">${city.weatherCondition}</p>
       </div>
@@ -67,23 +67,19 @@ export default class DashBoard {
    */
   createCityWidgetContent(cityData, key) {
     return `
-      <p class="city-info-grid__widget-description">${
-        cityData[key].name
+      <p class="city-info-grid__widget-description">${cityData[key].name
       }</p>
       <div class="city-info-grid__content-wrapper city-info-grid__content-wrapper--margin-bottom">
-          <p class="city-info-grid__widget-number">${
-            cityData[key].value
-          }</p>
-          ${
-            cityData[key].text
-              ? `<p class="city-info-grid__widget-data">${cityData[key].text}</p>`
-              : ""
-          }
-          ${
-            cityData[key].additional
-              ? `<p class="city-info-grid__widget-additional">${cityData[key].additional}</p>`
-              : ""
-          }
+          <p class="city-info-grid__widget-number">${cityData[key].value
+      }</p>
+          ${cityData[key].text
+        ? `<p class="city-info-grid__widget-data">${cityData[key].text}</p>`
+        : ""
+      }
+          ${cityData[key].additional
+        ? `<p class="city-info-grid__widget-additional">${cityData[key].additional}</p>`
+        : ""
+      }
       </div>
      `;
   }
