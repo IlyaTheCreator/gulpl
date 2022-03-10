81d87662260e63a91ce9c6296d66143399fadc4b
ilyaTheCreator <respondatanytime@gmail.com>
2022-03-10 08:38:14 +0300

    removed useless comments

--- app/js/index.js
+++ app/js/index.js
@@ -1,26 +1,3 @@
 import AppModule from "./modules/AppModule";
-import cities from "cities.json";
-
-// console.log(cities.find(city => city.name === "Moscow" && city.country === "RU"))
-
-// import WeatherAPIService from "./services/WeatherAPIService";
-
-// testing 
-// const weatherAPIService = new WeatherAPIService();
-
-// const testOpenWeather = async () => {
-//     const result = await weatherAPIService.openWeatherMapSearch();
-
-//     console.log(result);
-// }
-
-// const testFreeWeather = async () => {
-//     const result = await weatherAPIService.freeWeatherApiSearch();
-
-//     console.log(result);
-// }
-
-// testOpenWeather();
-// testFreeWeather();
 
 document.addEventListener("DOMContentLoaded", AppModule.init());
