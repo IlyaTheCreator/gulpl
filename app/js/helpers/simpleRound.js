823f28eb1730c3f6b4a51c6d132367375ab517b1
cache <respondatanytime@gmail.com>
2022-03-30 09:28:18 +0300

    added tests for helper functions

--- app/js/helpers/simpleRound.js
+++ app/js/helpers/simpleRound.js
@@ -1,8 +1,10 @@
 /**
  * simple number rounding by 1 decimal point after a comma
- * @param {number} num 
+ * @param {number} num
  * @returns {number}
  */
-export default function(num) {
-    return Math.round(num * 10) / 10;
+export default function (num) {
+  if (!num) return false;
+
+  return Math.round(num * 10) / 10;
 }
